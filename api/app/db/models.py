2397c72fc657dbaea45349f53ff87a7ddde1ba6c
Cary Carter <ccarterdev@gmail.com>
2023-10-04 21:10:18 -0700

    mapped columns

--- api/app/db/models.py
+++ api/app/db/models.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Integer, String, Column
-from sqlalchemy.orm import declarative_base, Mapped
+from typing import Optional
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-Base = declarative_base()
+Base = DeclarativeBase()
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = Column(
+    id: Mapped[int] = mapped_column(
         Integer, primary_key=True, index=True, autoincrement=True
     )
-    username: Mapped[str] = Column(String(50), unique=True, index=True)
-    hpassword: Mapped[str] = Column(String(100))
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    hpassword: Mapped[str] = mapped_column(String(255))
