6b35f3889327359f3b62fab6fa58b9bc293f947f
Cary Carter <ccarterdev@gmail.com>
2023-10-27 22:36:32 -0700

    common whatever

---
+++ api/tests/common.py
@@ -0,0 +1,62 @@
+import pytest
+from typing import Optional
+from datetime import datetime as dt
+from fastapi.testclient import TestClient
+from unittest.mock import AsyncMock, patch, Mock
+
+from api.app.schemas import user as SchemaUser, goal as SchemaGoal
+from api.app.main import app
+from api.app.utils.security import decode_jwt, hash_pass
+
+
+client = TestClient(app)
+
+
+USERNAME = "testuser"
+PASSWORD = "testpass"
+USERID = 1
+HPASSWORD = hash_pass(PASSWORD)
+
+LOGINJSON = {"username": USERNAME, "password": PASSWORD}
+
+USERDB = SchemaUser.UserDB(
+    id=USERID,
+    username=USERNAME,
+    hpassword=HPASSWORD,
+    created_on=dt.utcnow(),
+    updated_on=dt.utcnow(),
+)
+
+GOALS = [
+    SchemaGoal.GoalOut(
+        id=1,
+        text="test goal",
+        user_id=USERID,
+        created_on=dt.utcnow(),
+        updated_on=dt.utcnow(),
+        tasks=[],
+    ),
+    SchemaGoal.GoalOut(
+        id=2,
+        text="test goal 2",
+        user_id=USERID,
+        created_on=dt.utcnow(),
+        updated_on=dt.utcnow(),
+        tasks=[],
+    ),
+]
+
+
+def assert_token(resp):
+    assert resp.status_code == 200
+    assert "access_token" in resp.json()
+    assert resp.json()["token_type"] == "bearer"
+    _decoded = decode_jwt(resp.json()["access_token"])
+    assert "sub" in _decoded
+    assert _decoded["sub"] == LOGINJSON["username"]
+    assert 'exp' in _decoded
+
+
+@pytest.fixture(scope="function")
+async def curuser():
+    return USERDB
