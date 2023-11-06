839bf3a7b481fd357b37b8f71ecb4cef42d86850
Cary Carter <ccarterdev@gmail.com>
2023-11-05 18:18:41 -0800

    moved out of goal index into its own thing, should move into goalboard tbh

---
+++ frontend/src/pages/GoalsPage/actions.ts
@@ -0,0 +1,162 @@
+import React from "react";
+import apios from "../../apios";
+import { GoalOut, GoalTaskOut } from "../../api";
+
+export const actionUpdateGoal = async (
+  setGoals: React.Dispatch<React.SetStateAction<GoalOut[]>>,
+  goalId: number,
+  updatedText?: string,
+  updatedNotes?: string | null
+) => {
+  try {
+    let endpoint = `/goals/${goalId}`;
+    let payload: any = {};
+
+    if (updatedText !== undefined) {
+      payload.text = updatedText;
+    } else if (updatedNotes !== undefined) {
+      endpoint += `/notes`;
+      payload.notes = updatedNotes;
+    }
+
+    const response = await apios.put(endpoint, payload);
+
+    if (response.status === 200) {
+      setGoals((prevGoals) =>
+        prevGoals.map((g) => {
+          if (g.id !== goalId) return g;
+          if (updatedText !== undefined) return { ...g, text: updatedText };
+          if (updatedNotes !== undefined) return { ...g, notes: updatedNotes };
+          return g;
+        })
+      );
+      return true;
+    }
+  } catch (error) {
+    console.error(
+      `Error updating goal ${updatedText ? "text" : "notes"}:`,
+      error
+    );
+  }
+  return false;
+};
+
+export const actionGoalDelete = async (
+  goals: GoalOut[],
+  setGoals: React.Dispatch<React.SetStateAction<GoalOut[]>>,
+  goalId: number
+) => {
+  // prompt user if they are sure and only delete if so
+  const confirmed = window.confirm(
+    "Are you sure you want to delete this goal?"
+  );
+  if (!confirmed) {
+    return;
+  }
+  try {
+    const response = await apios.delete(`/goals/${goalId}`);
+    if (response.status === 200 || response.status === 204) {
+      // Assuming a successful delete returns 200 or 204 status
+      setGoals(goals.filter((goal) => goal.id !== goalId));
+    }
+  } catch (error) {
+    console.error("Error deleting goal:", error);
+  }
+};
+
+
+export const actionAddTaskToGoal = async (
+  goals: GoalOut[],
+  setGoals: React.Dispatch<React.SetStateAction<GoalOut[]>>,
+  goalId: number, taskText: string) => {
+  try {
+    const response = await apios.post<GoalTaskOut>(`/goals/${goalId}/tasks`, {
+      text: taskText,
+    });
+    if (response.data) {
+      setGoals(
+        goals.map((goal) => {
+          if (goal.id === goalId) {
+            const updatedTasks = goal.tasks
+              ? [...goal.tasks, response.data]
+              : [response.data];
+            return {
+              ...goal,
+              tasks: updatedTasks,
+            };
+          }
+          return goal;
+        })
+      );
+    }
+  } catch (error) {
+    console.error("Error adding task:", error);
+  }
+};
+
+export const actionDeleteTask = async (
+
+  goals: GoalOut[],
+  setGoals: React.Dispatch<React.SetStateAction<GoalOut[]>>,
+  goalId: number, taskId: number) => {
+  try {
+    const response = await apios.delete(`/goals/${goalId}/tasks/${taskId}`);
+    if (response.data) {
+      setGoals(
+        goals.map((goal) => {
+          if (goal.id === goalId) {
+            const updatedTasks = goal.tasks?.filter(
+              (task) => task.id !== taskId
+            );
+            return {
+              ...goal,
+              tasks: updatedTasks,
+            };
+          }
+          return goal;
+        })
+      );
+    } else {
+      console.error(
+        "Delete operation successful but unexpected response format: ",
+        response.data
+      );
+    }
+  } catch (error) {
+    console.error("Error deleting task:", error);
+  }
+};
+
+export   const actionTaskCompletion = async (
+  goals: GoalOut[],
+  setGoals: React.Dispatch<React.SetStateAction<GoalOut[]>>,
+  goalId: number,
+  taskId: number,
+  isCompleted: boolean
+) => {
+  try {
+    const response = await apios.put(`/goals/${goalId}/tasks/${taskId}`, {
+      completed: !isCompleted,
+    });
+    if (response.data) {
+      setGoals(
+        goals.map((goal) => {
+          if (goal.id === goalId && goal.tasks) {
+            goal.tasks = goal.tasks.map((task) => {
+              if (task.id === taskId) {
+                return {
+                  ...task,
+                  completed: !isCompleted,
+                };
+              }
+              return task;
+            });
+          }
+          return goal;
+        })
+      );
+    }
+  } catch (error) {
+    console.error("Error updating task completion status:", error);
+  }
+};
--- frontend/src/pages/GoalsPage/index.tsx
+++ frontend/src/pages/GoalsPage/index.tsx
@@ -5,6 +5,12 @@ import { GoalOut, GoalTaskOut } from "../../api";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import GoalBoard from "./GoalBoard";
+import {actionUpdateGoal,
+  actionDeleteTask,
+  actionAddTaskToGoal,
+  actionTaskCompletion,
+  actionGoalDelete,
+} from "./actions";
 
 const GoalsPage: React.FC = () => {
   const [goals, setGoals] = useState<GoalOut[]>([]);
@@ -54,75 +60,14 @@ const GoalsPage: React.FC = () => {
     taskId: number,
     isCompleted: boolean
   ) => {
-    try {
-      const response = await apios.put(`/goals/${goalId}/tasks/${taskId}`, {
-        completed: !isCompleted,
-      });
-      if (response.data) {
-        setGoals(
-          goals.map((goal) => {
-            if (goal.id === goalId && goal.tasks) {
-              goal.tasks = goal.tasks.map((task) => {
-                if (task.id === taskId) {
-                  return {
-                    ...task,
-                    completed: !isCompleted,
-                  };
-                }
-                return task;
-              });
-            }
-            return goal;
-          })
-        );
-      }
-    } catch (error) {
-      console.error("Error updating task completion status:", error);
-    }
+    return actionTaskCompletion(goals, setGoals, goalId, taskId, isCompleted)
   };
   const handleAddTaskToGoal = async (goalId: number, taskText: string) => {
-    try {
-      const response = await apios.post<GoalTaskOut>(`/goals/${goalId}/tasks`, {
-        text: taskText,
-      });
-      if (response.data) {
-        setGoals(
-          goals.map((goal) => {
-            if (goal.id === goalId) {
-              const updatedTasks = goal.tasks
-                ? [...goal.tasks, response.data]
-                : [response.data];
-              return {
-                ...goal,
-                tasks: updatedTasks,
-              };
-            }
-            return goal;
-          })
-        );
-      }
-    } catch (error) {
-      console.error("Error adding task:", error);
-    }
+    return actionAddTaskToGoal(goals, setGoals, goalId, taskText)
   };
 
   const handleGoalDelete = async (goalId: number) => {
-    // prompt user if they are sure and only delete if so
-    const confirmed = window.confirm(
-      "Are you sure you want to delete this goal?"
-    );
-    if (!confirmed) {
-      return;
-    }
-    try {
-      const response = await apios.delete(`/goals/${goalId}`);
-      if (response.status === 200 || response.status === 204) {
-        // Assuming a successful delete returns 200 or 204 status
-        setGoals(goals.filter((goal) => goal.id !== goalId));
-      }
-    } catch (error) {
-      console.error("Error deleting goal:", error);
-    }
+    return actionGoalDelete(goals, setGoals, goalId)
   };
 
   const handleGoalUpdate = async (
@@ -130,67 +75,11 @@ const GoalsPage: React.FC = () => {
     updatedText?: string,
     updatedNotes?: string | null
   ) => {
-    try {
-      let endpoint = `/goals/${goalId}`;
-      let payload: any = {};
-
-      if (updatedText !== undefined) {
-        payload.text = updatedText;
-      } else if (updatedNotes !== undefined) {
-        endpoint += `/notes`;
-        payload.notes = updatedNotes;
-      }
-
-      const response = await apios.put(endpoint, payload);
-
-      if (response.status === 200) {
-        setGoals((prevGoals) =>
-          prevGoals.map((g) => {
-            if (g.id !== goalId) return g;
-            if (updatedText !== undefined) return { ...g, text: updatedText };
-            if (updatedNotes !== undefined)
-              return { ...g, notes: updatedNotes };
-            return g;
-          })
-        );
-        return true;
-      }
-    } catch (error) {
-      console.error(
-        `Error updating goal ${updatedText ? "text" : "notes"}:`,
-        error
-      );
-    }
-    return false;
+    return actionUpdateGoal(setGoals, goalId, updatedText, updatedNotes)
   };
 
   const handleDeleteTask = async (goalId: number, taskId: number) => {
-    try {
-      const response = await apios.delete(`/goals/${goalId}/tasks/${taskId}`);
-      if (response.data) {
-        setGoals(
-          goals.map((goal) => {
-            if (goal.id === goalId) {
-              const updatedTasks = goal.tasks?.filter(
-                (task) => task.id !== taskId
-              );
-              return {
-                ...goal,
-                tasks: updatedTasks,
-              };
-            }
-            return goal;
-          })
-        );
-      } else {
-        console.error(
-          "Delete operation successful but unexpected response format: ",
-          response.data
-        );
-      }
-    } catch (error) {
-      console.error("Error deleting task:", error);
-    }
+    return actionDeleteTask(goals, setGoals, goalId, taskId)
   };
 
   return (
