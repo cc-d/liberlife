cb7c1c6d2842df74bad245606fa9e3cbb8fabf04
Cary Carter <ccarterdev@gmail.com>
2023-11-18 15:37:26 -0800

    mobile

---
+++ mobile/app/src/main/java/com/example/liberlife/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.liberlife;
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebResourceError;
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.loadUrl(
+            "https://life.liberfy.ai/"
+        )
+
+
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+                // Log or handle errors
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+
+        WebView.setWebContentsDebuggingEnabled(true)
+    }
+}
+
+
