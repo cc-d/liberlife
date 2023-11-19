554d52917d3a9b17b766c897d139dd140aab0fe9
Cary Carter <ccarterdev@gmail.com>
2023-11-19 14:38:55 -0800

    used parameterize for better tests

--- api/tests/unit/test_httperrors.py
+++ api/tests/unit/test_httperrors.py
@@ -5,76 +5,30 @@ from fastapi import status
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 
-from api.app.utils import httperrors as ERR
-
 _STR = 'test string'
 
 
-def _errmatch(err1, err2):
-    assert err1.status_code == err2.status_code
-    assert err1.detail == err2.detail
-
-
 @pytest.mark.asyncio
-async def test_400():
-    _errmatch(ERR.HTTP400(_STR), HTTPException(400, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_401():
-    _errmatch(ERR.HTTP401(_STR), HTTPException(401, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_403():
-    _errmatch(ERR.HTTP403(_STR), HTTPException(403, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_404():
-    _errmatch(ERR.HTTP404(_STR), HTTPException(404, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_409():
-    _errmatch(ERR.HTTP409(_STR), HTTPException(409, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_422():
-    _errmatch(ERR.HTTP422(_STR), HTTPException(422, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_500():
-    _errmatch(ERR.HTTP500(_STR), HTTPException(500, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_501():
-    _errmatch(ERR.HTTP501(_STR), HTTPException(501, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_502():
-    _errmatch(ERR.HTTP502(_STR), HTTPException(502, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_503():
-    _errmatch(ERR.HTTP503(_STR), HTTPException(503, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_504():
-    _errmatch(ERR.HTTP504(_STR), HTTPException(504, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_505():
-    _errmatch(ERR.HTTP505(_STR), HTTPException(505, detail=_STR))
-
-
-@pytest.mark.asyncio
-async def test_507():
-    _errmatch(ERR.HTTP507(_STR), HTTPException(507, detail=_STR))
+@pytest.mark.parametrize(
+    'custom_exception, status_code',
+    [
+        (ERR.HTTP400, status.HTTP_400_BAD_REQUEST),
+        (ERR.HTTP401, status.HTTP_401_UNAUTHORIZED),
+        (ERR.HTTP403, status.HTTP_403_FORBIDDEN),
+        (ERR.HTTP404, status.HTTP_404_NOT_FOUND),
+        (ERR.HTTP409, status.HTTP_409_CONFLICT),
+        (ERR.HTTP422, status.HTTP_422_UNPROCESSABLE_ENTITY),
+        (ERR.HTTP500, status.HTTP_500_INTERNAL_SERVER_ERROR),
+        (ERR.HTTP501, status.HTTP_501_NOT_IMPLEMENTED),
+        (ERR.HTTP502, status.HTTP_502_BAD_GATEWAY),
+        (ERR.HTTP503, status.HTTP_503_SERVICE_UNAVAILABLE),
+        (ERR.HTTP504, status.HTTP_504_GATEWAY_TIMEOUT),
+        (ERR.HTTP505, status.HTTP_505_HTTP_VERSION_NOT_SUPPORTED),
+        (ERR.HTTP507, status.HTTP_507_INSUFFICIENT_STORAGE),
+    ],
+)
+async def test_http_exceptions(custom_exception, status_code):
+    custom_error = custom_exception(_STR)
+    expected_error = HTTPException(status_code, detail=_STR)
+    assert custom_error.status_code == expected_error.status_code
+    assert custom_error.detail == expected_error.detail
