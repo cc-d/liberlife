78c136d75a20cd546c033253dcdfa730c5b46709
Cary Carter <ccarterdev@gmail.com>
2023-12-10 10:35:04 -0800

    shell.sh ...

--- shell.sh
+++ shell.sh
@@ -119,6 +119,7 @@ movetowww() {
     sudo mv "$ROOTDIR/nginx/html" "/var/www/html"
     echo "substituting urls for $LIBLIFE_ENV"
     for f in $(find /var/www/html/static -type f -name "*.js"); do
+        echo "substituting $f"
         sudo sed -i "s/http:\/\/localhost:8999/https:\/\/$REACT_APP_HOST:$REACT_APP_PORT/g" $f
     done
     sudo systemctl restart nginx
