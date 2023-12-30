9d4d9c7d5f9bfe1c4d185d4aebbd808cc29c19cf
Cary Carter <ccarterdev@gmail.com>
2023-12-29 16:17:01 -0800

    frontend/public/build.txt nginx/html/build.txt shell.sh ...

--- shell.sh
+++ shell.sh
@@ -134,7 +134,7 @@ movetowww() {
     _WWW_NGINXHTML="/var/www/html"
 
     [ -d "$_WWW_NGINXHTML" ] && \
-        echo "nginx html exists at nginx/html deleting" && \
+        echo "nginx html exists at $_WWW_REPOHTML deleting" && \
         sudo rm -r "$_WWW_REPOHTML" && \
         mkdir $_WWW_REPOHTML
 
