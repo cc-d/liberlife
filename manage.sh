c99297bf3bffa764760cbcddc3bc95b1dc33e9c1
Cary Carter <ccarterdev@gmail.com>
2025-05-01 00:34:27 -0500

    stopping midwya thru bc late time"

--- manage.sh
+++ manage.sh
@@ -16,6 +16,8 @@ for l in $(cat "$API_DIR/envs/$LIBLIFE_ENV.env" | grep -vE "^$"); do
     export $l;
 done
 
+
+
 uvistart() {
     if [ -d "$API_DIR/venv" ]; then
         . "$API_DIR/venv/bin/activate"
@@ -28,53 +30,54 @@ for l in $(cat "$FRONT_DIR/envs/$LIBLIFE_ENV.env" | grep -vE "^$"); do
     export $l;
 done
 
+export dcexec-front() {
+    echo "$dcexec cmd: $@"
+    shift
+    docker compose exec -it frontend /bin/bash "$@"
+}
+
+API_BASE="http://$API_HOST:$API_PORT"
+OPEN_API_URL="$API_BASE/openapi.json"
 
 build_types() {
-
-    API_BASE="http://localhost:$API_PORT"
-    OPEN_API_URL="$API_BASE/openapi.json"
     echo "Generating types from: $OPEN_API_URL"
 
-    npx openapi-typescript-codegen generate \
+    dcexec-front $npx openapi-typescript-codegen generate \
         --exportSchemas true \
         --input "$OPEN_API_URL" \
         --output "$ROOT_DIR/frontend/src/api/"
+
+
+
 }
 
 
 build_frontend() {
     # NVM init
-    if [ -d "$HOME/.nvm" ]; then
-        export NVM_DIR="$HOME/nvm.sh";
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    else
-        echo "nvm/node 20 is required to build the frontend without docker"
-    fi
+    build_types
 
-    nvm use 20;
-
-    npm ci --omit=dev
+    dcexec-front npm ci --omit=dev
 
     echo "$(date)" > "$FRONT_DIR/build.txt"
     echo "$(git rev-parse HEAD)" >> "$FRONT_DIR/build.txt"
 
-    if [ "$REACT_APP_API_BASEURL" = "https://life.liberfy.ai/api" ]; then
-        FIX_URLS="$BASE"
-        REP_URLS="https://life.liberfy.ai/api"
-        sed -i.bak "s|$FIX_URLS|$REP_URLS|g" "$ROOT_DIR/nginx/html/static/js/*.js"
+    PROD_API_URL="https://life.liberfy.ai/api"
+
+    if [ "$REACT_APP_API_BASEURL" = $PROD_API_URL ]; then
+        sed -i.bak "s|$$API_BASE|$PROD_API_URL|g" "$ROOT_DIR/nginx/html/static/js/*.js"
     fi
 
 }
 
 while [ $# -gt 0 ]; do
     case $1 in
-        build_types)
+        build_frontend)
+            build_frontend
             shift
-            build_types
             ;;
         build_types)
-            shift
             build_types
+            shift
             ;;
       *)
         echo "Invalid option: $1" >&2
